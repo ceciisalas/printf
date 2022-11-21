a7eeced841b9e9bf4c30672f24134d53a3934081
ceciisalas <cecizs1@hotmail.com>
2022-11-21 17:12:40 +0000

    get the right function and return expected function

---
+++ get_func.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *get_func - select the correct function to return
+ * the expected output
+ * @s: pointer to string
+ * Return: correct function
+ */
+int (*get_func)(char *s)(va_list)
+{
+        va_list arg;
+
+        format_p ops[] = {
+                {'c', print_char},
+                {'s', print_str},
+        };
+
+        i = 0;
+        while (i < 2)
+        {
+                if (*ops[i].c == *s)
+		{
+			return (format_p[i].print_fmt);
+		}
+	}
+	return (NULL);
+}
