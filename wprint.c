613d961ec6cbf1ef6d5014519450138163f9c38e
ceciisalas <cecizs1@hotmail.com>
2022-11-22 19:02:13 +0000

    print the string

--- wprint.c
+++ wprint.c
@@ -14,11 +14,13 @@ int wprint(const char *format, va_list arg)
 	flagCount = 0;
 	for (i = 0; format[i] != 0; i++)
 	{
-		if (format[i] == '%' && format[i + 1] == '%')
+		if (format[i] == '%')
 		{
-
+			if (format[i + 1] == '%')
+			{
 			flagCount += _putchar(format[i]);
 			i++;
+			}
 		}
 		else if (format[i + 1] != 0)
 		{
